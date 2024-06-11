1b3e0c514e442435c2263f9f325d4a926d603b3e
Dede Fuji Abdul <defuj.official@gmail.com>
2024-06-11 09:38:00 +0700

    feat: Add validation functions for email, password, name, phone, url, and image
    This commit adds validation functions for email, password, name, phone, url, and image to the `text.ts` file in the `utils` directory. These functions enhance the utility of the `text.ts` module by providing validation checks for different types of input data. The functions `isEmailValid`, `isPasswordValid`, `isNameValid`, `isPhoneValid`, `isUrlValid`, and `isImageValid` are added to validate email addresses, passwords, names, phone numbers, URLs, and image file extensions, respectively.

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -16,3 +16,32 @@ export const formatCurrency = (amount: number) => {
 export const formatNumber = (amount: number) => {
   return amount.toLocaleString('en-US')
 }
+export const isEmailValid = (email: string) => {
+  const re = /\S+@\S+\.\S+/
+  return re.test(email)
+}
+
+export const isPasswordValid = (password: string) => {
+  const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/
+  return re.test(password)
+}
+
+export const isNameValid = (name: string) => {
+  const re = /^[a-zA-Z\s]*$/
+  return re.test(name)
+}
+
+export const isPhoneValid = (phone: string) => {
+  const re = /^[0-9]*$/
+  return re.test(phone)
+}
+
+export const isUrlValid = (url: string) => {
+  const re = /^(ftp|http|https):\/\/[^ "]+$/
+  return re.test(url)
+}
+
+export const isImageValid = (image: string) => {
+  const re = /\.(gif|jpe?g|tiff?|png|webp|bmp)$/i
+  return re.test(image)
+}

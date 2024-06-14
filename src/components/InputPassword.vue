0c3e31b36e3efcec012f44f5396a3db5a0990dc3
WfQinthara <wildanfathir99@gmail.com>
2024-06-14 17:23:42 +0700

    fix: menambangkan lang:ts pada setiap script setup

--- src/components/InputPassword.vue
+++ src/components/InputPassword.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 const isPasswordVisible = ref(false)
 const password = ref('')
 
--- src/components/charts/ChartBar.vue
+++ src/components/charts/ChartBar.vue
@@ -2,7 +2,7 @@
   <Bar :data="chartData" :options="chartOptions" />
 </template>
 
-<script>
+<script >
 import { Bar } from 'vue-chartjs'
 import { Chart as ChartJS, BarElement, CategoryScale, LinearScale } from 'chart.js'
 

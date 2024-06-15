a598278d58273c1f7d2a7d7ae734d2478cc98d92
WfQinthara <wildanfathir99@gmail.com>
2024-06-15 16:56:16 +0700

    Fix: Memperbaiki bug dan error pada PopUpResto

--- src/components/dialogs/PopUpResto.vue
+++ src/components/dialogs/PopUpResto.vue
@@ -2,14 +2,17 @@
 import { Dialog, DialogPanel, TransitionChild, TransitionRoot } from '@headlessui/vue'
 import { computed, ref } from 'vue'
 import { XMarkIcon } from '@heroicons/vue/24/outline'
-import { useRestoStore } from '@/stores/resto'
+// import { useRestoStore } from '@/stores/resto'
 import type { RestaurantModel } from '@/utils/types'
 import { UpDownIcon } from '@/components/icons'
 
 const props = defineProps({
   open: Boolean,
-  data: Object as () => RestaurantModel
+  data: Object as () => RestaurantModel | null
 })
+
+const currentPackageClass = ref('');
+
 const emits = defineEmits(['onClose', 'onSelected'])
 const open = ref(computed(() => props.open))
 const data = ref(computed(() => props.data))
@@ -73,40 +76,39 @@ function close() {
                           class="flex items-center space-x-2 mb-4 pb-6 border-b border-dotted border-black w-full"
                         >
                           <img
-                            :src="data.resto.resto_image"
+                            :src="data?.resto.resto_image"
                             alt="Restaurant Image"
                             class="w-[74px] h-[74px] rounded-lg object-cover"
                           />
                           <div class="space-y-2">
                             <div class="items-center">
                               <div class="flex items-center space-x-1.5">
-                                <!-- Tambahkan kelas space-x-2 untuk jarak horizontal -->
                                 <div
-                                  v-if="data.account.account_subscription_id == 0"
+                                  v-if="data?.account.account_subscription_id == 0"
                                   class="flex p-1 items-start gap-2.5 rounded-full bg-primary-50"
                                 >
                                   <p class="text-xs text-primary-800">Free</p>
                                 </div>
                                 <div
-                                  v-else-if="data.account.account_subscription_id == 1"
+                                  v-else-if="data?.account.account_subscription_id == 1"
                                   class="flex p-1 items-start gap-2.5 rounded-full bg-primary-50"
                                 >
                                   <p class="text-xs text-primary-800">Trial</p>
                                 </div>
                                 <div
-                                  v-else-if="data.account.account_subscription_id > 1"
+                                  v-else-if="data && data.account && data.account.account_subscription_id && data.account.account_subscription_id > 1"
                                   class="flex p-1 items-start bg-gradient-to-r from-[#E3FFD3] to-[#FFF59C] rounded-full space-x-1"
                                 >
                                   <p class="text-xs text-primary-900">Premium</p>
                                 </div>
                                 <div
-                                  v-if="data.account.account_subscription == true"
+                                  v-if="data?.account.account_subscription == true"
                                   class="flex p-1 items-center gap-2.5"
                                 >
                                   <p class="text-xs text-superorange">Aktif</p>
                                 </div>
                                 <div
-                                  v-else-if="data.account.account_subscription == false"
+                                  v-else-if="data?.account.account_subscription == false"
                                   class="flex p-1 items-center gap-2.5"
                                 >
                                   <p class="text-xs text-red-500">Tidak Aktif</p>
@@ -114,9 +116,9 @@ function close() {
                               </div>
 
                               <div class="space-y-1">
-                                <h2 class="text-2xl font-bold">{{ data.resto.resto_name }}</h2>
+                                <h2 class="text-2xl font-bold">{{ data?.resto.resto_name }}</h2>
                                 <p class="text-xs text-gray-600 font-medium">
-                                  {{ data.resto.resto_id }}
+                                  {{ data?.resto.resto_id }}
                                 </p>
                               </div>
                             </div>
@@ -129,7 +131,7 @@ function close() {
                                 >Jumlah Menu</label
                               >
                               <p class="text-sm text-dark font-medium">
-                                {{ data.resto.resto_menu }}
+                                {{ data?.resto.resto_menu }}
                               </p>
                             </div>
                             <div class="py-2 px-4 bg-gray-50 rounded-lg space-y-2">
@@ -137,23 +139,23 @@ function close() {
                                 >Transaksi Hari Ini</label
                               >
                               <p class="text-sm text-dark font-medium">
-                                {{ data.resto.resto_transaction_today }}
+                                {{ data?.resto.resto_transaction_today }}
                               </p>
                             </div>
                           </div>
                           <div class="mb-4 space-y-1">
                             <label class="block text-sm font-medium text-gray-700">Alamat</label>
                             <p class="text-sm text-dark font-medium">
-                              {{ data.resto.resto_address }}
+                              {{ data?.resto.resto_address }}
                             </p>
                           </div>
-                          <div class="mb-4 flex items-end justify-between">
+                          <div class="mb-4 flex items-end justify-between" v-if="data">
                             <div class="space-y-1">
                               <label class="block text-sm font-medium text-gray-700"
                                 >No Whatsapp</label
                               >
                               <p class="text-sm font-medium text-dark">
-                                {{ data.resto.resto_contact }}
+                                {{ data.resto?.resto_contact }}
                               </p>
                             </div>
                             <a
@@ -166,15 +168,15 @@ function close() {
                           <div class="mb-4 space-y-1.5 border-b border-dotted border-black pb-4">
                             <label class="block text-sm font-medium text-gray-700">Email</label>
                             <p class="text-sm font-medium text-dark">
-                              {{ data.account.account_email }}
+                              {{ data?.account.account_email }}
                             </p>
                           </div>
-                          <div class="mb-4 space-y-1">
+                          <div class="mb-4 space-y-1" v-if="data && data.account">
                             <label class="block text-sm font-medium text-gray-700"
                               >Paket Saat Ini</label
                             >
                             <p
-                              class="text-xs py-1 px-2 bg-gradient-to-r from-[#E3FFD3] to-[#FFF59C] rounded-full space-x-1 w-max rounded-lg font-bold"
+                              class="text-xs py-1 px-2 bg-gradient-to-r from-[#E3FFD3] to-[#FFF59C] rounded-full space-x-1 w-max font-bold"
                               :class="currentPackageClass"
                             >
                               {{ data.account.account_subscription_name }} |

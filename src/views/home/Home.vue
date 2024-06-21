250b0dab26c0afce50c1762ab083c4a18ab342eb
Dede Fuji Abdul <defuj.official@gmail.com>
2024-06-21 16:17:21 +0700

    refactor: Update main content styles for better responsiveness

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -15,9 +15,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <section
-    class="flex items-start justify-center w-full rounded-lg md:p-6 lg:bg-white md:rounded-3xl"
-  >
+  <section class="main-content">
     <div
       class="flex flex-col gap-6 mb-[100px] md:mb-0 xl:items-start lg:justify-between xl:flex-row w-full"
     >

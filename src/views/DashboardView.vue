c3661adb88067a9884ab445ee64007fd437b72da
WFQinthara <wildanfathir99@gmail.com>
2024-06-21 15:55:56 +0700

    git

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import { useAccountStore } from '@/stores/account'
 import { Navigasi, Header } from '@/components'
-
-const account = useAccountStore()
 </script>
 
 <template>

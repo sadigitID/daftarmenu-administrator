c92419795c34a2fc2f98997606734a6698f0ad91
WFQinthara <wildanfathir99@gmail.com>
2024-06-27 15:06:35 +0700

    git

--- src/components/dialogs/previewNote.vue
+++ src/components/dialogs/previewNote.vue
@@ -4,8 +4,8 @@ import { computed, ref, watch } from 'vue'
 import { XMarkIcon } from '@heroicons/vue/24/outline'
 import { Switch } from '@headlessui/vue'
 import type { NoteModel } from '@/utils/types'
-import { Check, Block } from "@/components/icons";
-import { EditNote } from "@/components/dialogs";
+import { Check, Block } from '@/components/icons'
+import { EditNote } from '@/components/dialogs'
 const props = defineProps({
   open: Boolean,
   data: Object as () => NoteModel | null
@@ -20,13 +20,15 @@ function close() {
 
 const enabled = ref(props.data?.detail.status === 'Selesai')
 
-
-watch(() => props.data, (newData) => {
-  if (newData) {
-    enabled.value = newData.detail.status === 'Selesai'
-  }
-}, { immediate: true })
-
+watch(
+  () => props.data,
+  (newData) => {
+    if (newData) {
+      enabled.value = newData.detail.status === 'Selesai'
+    }
+  },
+  { immediate: true }
+)
 
 watch(enabled, (newValue) => {
   if (props.data) {
@@ -43,12 +45,10 @@ const openEditNote = () => {
 const closeEditNote = () => {
   showEditNotePopup.value = false
 }
-
 </script>
 
-
 <template>
-    <EditNote :open="showEditNotePopup" @on-close="closeEditNote" />
+  <EditNote :open="showEditNotePopup" @on-close="closeEditNote" />
   <TransitionRoot as="template" :show="open">
     <Dialog as="div" class="relative z-20" @close="close">
       <TransitionChild
@@ -121,17 +121,15 @@ const closeEditNote = () => {
                                 <span class="sr-only">Toggle</span>
                                 <span
                                   :class="enabled ? 'translate-x-6' : 'translate-x-1'"
-                                  class="inline-block h-4 w-4 transform rounded-full bg-white transition-transform duration-300">
-                             
-                             
-                                <Check v-if="enabled" class="h-4 w-4 text-vtd-primary-500"/>
-                              <Block v-else class="h-4 w-4 text-vtd-primary-500" />
-                            </span>
+                                  class="inline-block h-4 w-4 transform rounded-full bg-white transition-transform duration-300"
+                                >
+                                  <Check v-if="enabled" class="h-4 w-4 text-vtd-primary-500" />
+                                  <Block v-else class="h-4 w-4 text-vtd-primary-500" />
+                                </span>
                               </Switch>
-                             
+
                               <span>{{ enabled ? 'Selesai' : 'Belum Selesai' }}</span>
                               <!-- Icon based on enabled state -->
-                        
                             </div>
                           </div>
                         </div>
@@ -145,20 +143,23 @@ const closeEditNote = () => {
                           <span class="label text-sm text-gray-"> Gambar Pendukung </span>
                           <div class="preview-img flex items-start flex-wrap gap-1">
                             <img
-                            :src="data?.detail.img"
-                            class="img w-[128px] h-[128px] bg-gray-50 rounded-md"/>
+                              :src="data?.detail.img"
+                              class="img w-[128px] h-[128px] bg-gray-50 rounded-md"
+                            />
                             <img
-                            :src="data?.detail.img"
-                            class="img w-[128px] h-[128px] bg-gray-50 rounded-md"/>
-                                <img
-                            :src="data?.detail.img"
-                            class="img w-[128px] h-[128px] bg-gray-50 rounded-md"/>
+                              :src="data?.detail.img"
+                              class="img w-[128px] h-[128px] bg-gray-50 rounded-md"
+                            />
+                            <img
+                              :src="data?.detail.img"
+                              class="img w-[128px] h-[128px] bg-gray-50 rounded-md"
+                            />
                           </div>
                         </div>
                         <div class="absolute bottom-0 left-0 w-full p-4 sm:p-6">
                           <div class="flex action-btn gap-2">
                             <Button
-                            @click="openEditNote"
+                              @click="openEditNote"
                               class="cursor-pointer w-[50%] text-center py-2 rounded-md bg-vtd-primary-500 text-white font-medium hover:bg-vtd-primary-600 transition-all duration-300"
                             >
                               Edit Catatan
@@ -182,4 +183,3 @@ const closeEditNote = () => {
     </Dialog>
   </TransitionRoot>
 </template>
-
